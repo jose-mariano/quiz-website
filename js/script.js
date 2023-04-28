a75e331a16a1b412b5d8fcf1ddad62e7a0676d4f
José Mariano <jose.mariano2002@outlook.com>
2023-04-27 22:50:20 -0300

    feat: Finish result page style
    Co-authored-by: ingridmcardoso <ingridmcardoso@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -22,6 +22,42 @@ function main() {
     showQuestions(showQuizResult)
   })
 
+  const btnShare = document.querySelector('#btn-share')
+  btnShare.addEventListener('click', () => {
+    console.log('teste')
+  })
+
+  const btnWhatsapp = document.querySelector('#btn-whatsapp')
+  btnWhatsapp.addEventListener(
+    'click',
+    () => openNewWindow('https://api.whatsapp.com/send?text=http://localhost:8080'),
+  )
+
+  const btnTwitter = document.querySelector('#btn-twitter')
+  btnTwitter.addEventListener(
+    'click',
+    () => openNewWindow('https://twitter.com/intent/tweet?text=http://localhost:8080'),
+  )
+
+  const btnFacebook = document.querySelector('#btn-facebook')
+  btnFacebook.addEventListener(
+    'click',
+    () => openNewWindow('https://www.facebook.com/sharer/sharer.php?u=http://localhost:8080'),
+  )
+
+  const copyToClipboardButton = document.querySelector('#copy-to-clipboard')
+  copyToClipboardButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(location.href)
+  })
+
+  function openNewWindow(url) {
+    window.open(
+      url,
+      '_blank',
+      'width=800,height=600',
+    )
+  }
+
   function showQuizResult(answers) {
     containerQuestions.classList.remove('element-active')
     containerResult.classList.add('element-active')
