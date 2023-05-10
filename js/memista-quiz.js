76c6426ade588ed4c26cd9e592eb698d9a84506a
José Mariano <jose.mariano2002@outlook.com>
2023-05-09 22:48:20 -0300

    feat: Add four question in the meme quiz
    Co-authored-by: ingridmcardoso <ingridmcardoso@users.noreply.github.com>

--- js/memista-quiz.js
+++ js/memista-quiz.js
@@ -2,11 +2,43 @@ function main() {
   const questions = [
     {
       number: '1',
-      answer: 'a'
+      answer: 'b'
     },
     {
       number: '2',
       answer: 'b'
+    },
+    {
+      number: '3',
+      answer: 'a'
+    },
+    {
+      number: '4',
+      answer: 'c'
+    },
+    {
+      number: '5',
+      answer: ''
+    },
+    {
+      number: '6',
+      answer: ''
+    },
+    {
+      number: '7',
+      answer: ''
+    },
+    {
+      number: '8',
+      answer: ''
+    },
+    {
+      number: '9',
+      answer: ''
+    },
+    {
+      number: '10',
+      answer: ''
     }
   ]
 
